b427d053cc1b85026387c41ad4d04743a9cf6b52
nailyk <jenkins@nailyk.fr>
2019-05-05 18:54:22 +0200

    lilac: NFC: Add nxp secure_element config
    Change-Id: Ic83f425f8b8ee36678c38ebbbf4bf8846cbebcfd
    Signed-off-by: nailyk <jenkins@nailyk.fr>

--- device/config-files.mk
+++ device/config-files.mk
@@ -2,7 +2,8 @@
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(DEVICE_PATH)/config/nfc/libnfc-nxp-octopus.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-octopus.conf \
-    $(DEVICE_PATH)/config/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    $(DEVICE_PATH)/config/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    hardware/nxp/secure_element/libese-spi/p73/libese-nxp-P73.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf
 
 ### Audio
 PRODUCT_COPY_FILES += \
